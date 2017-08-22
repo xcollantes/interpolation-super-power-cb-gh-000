8190eb952575fd1bcdb6aa2aba3a17df036a8790
Xavier Collantes <xcollantes@zagmail.gonzaga.edu>
2017-08-22 21:53:39 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -4,11 +4,11 @@ def display_rainbow(colors)
 
   puts "First color is: #{colors[0]}"
   puts "Second color is: #{colors[1]}"
-  puts "Thrid color is: #{colors[2]}"
-  puts "Thrid color is: #{colors[3]}"
-  puts "Thrid color is: #{colors[4]}"
-  puts "Thrid color is: #{colors[5]}"
-  puts "Thrid color is: #{colors[6]}"
+  puts "Third color is: #{colors[2]}"
+  puts "Fourth color is: #{colors[3]}"
+  puts "Fifth color is: #{colors[4]}"
+  puts "Sixth color is: #{colors[5]}"
+  puts "Seveth color is: #{colors[6]}"
   #puts "Thrid color is: #{colors[7]}"
 end
 
